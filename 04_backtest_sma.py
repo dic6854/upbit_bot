b3812b6009edce175e90cb2382203d168c212f4c
Seyong Jeon <dic104@naver.com>
2025-03-19 16:59:05 +0900

    upbit_bot 2025-03-19 16:58

--- 04_backtest_sma.py
+++ 04_backtest_sma.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 from datetime import datetime
 import talib
+import calendar
 
 def get_price(buy_time, df):
     while True:
@@ -14,27 +15,16 @@ def get_price(buy_time, df):
             buy_time += pd.Timedelta(minutes=1)
 
 def set_volume(capital, price):
-    unit = capital / price
+    unit = int((capital / price) * 10000) / 10000
 
     while True:
-        volume = capital - price * unit - (price * unit) * 0.0005
+        volume = price * unit + price * unit * 0.0005
 
         if float(volume) < float(capital):
-            return (unit-0.00001)
+            return unit
         unit -= 0.0001
 
 if __name__ == "__main__":
-    # 초기 자본금 및 리스크 설정
-    initial_capital = 1000000.0  # 100만원
-
-    # 매매 로직 구현
-    position = 0  # 0: 보유 없음, 1: 보유 있음
-    trades = []
-    capital = initial_capital
-    myprofits = []
-    volume = 0.001
-    curr_profit = 0.0
-    flag = False
 
     krw_tickers = pyupbit.get_tickers(fiat="KRW")
 
@@ -42,15 +32,10 @@ if __name__ == "__main__":
     ct = 1
     for i in range(tt):
         ticker = krw_tickers[i]
-        # file_name_m1 = f"test/{ticker}_m1.csv"
-        # file_name_m5 = f"test/{ticker}_m5.csv"
-        # trade_output = f"test/backtest/{ticker}_trade_m5.csv"
-        # my_profit_output = f"test/backtest/{ticker}_myprofit_m5.csv"
-
-        file_name_m1 = f"cdata/{ticker}_m1.csv"
-        file_name_m5 = f"cdata/{ticker}_m5.csv"
-        trade_output = f"backtest/{ticker}_trade_m5.csv"
-        profit_output = f"backtest/{ticker}_profit_m5.csv"
+        file_name_m1 = f"test/{ticker}_m1.csv"
+        file_name_m5 = f"test/{ticker}_m5.csv"
+        trade_output = f"test/backtest/{ticker}_trade_m5.csv"
+        profit_output = f"test/backtest/{ticker}_profit_m5.csv"
 
         df_m1 = pd.read_csv(file_name_m1, index_col=0)
         df_m5 = pd.read_csv(file_name_m5, index_col=0)
@@ -65,78 +50,116 @@ if __name__ == "__main__":
         except:
             df_m5.index = pd.to_datetime(df_m5.index, format="%Y-%m-%d %H:%M")
 
+        # 초기 자본금 및 리스크 설정
+        initial_capital = 1000000.0  # 초기 자본 : 100만원
+        position = 0  # 0: 보유 없음, 1: 보유 있음
+        trades = []
+        profits = []
+        capital = initial_capital
+        volume = 0.001
+        curr_profit = 0.0
+        flag = False
+
         start_datetime = df_m5.index[0]
-        start_datetime = start_datetime.replace(hour=8, minute=55, second=0)
         end_datetime = df_m5.index[-1]
 
-        curr_datetime = start_datetime
+        current_year = start_datetime.year
+        gcount = 0
+        while current_year <= end_datetime.year:
+            current_month = start_datetime.month if current_year == start_datetime.year else 1
 
-        while curr_datetime <= end_datetime:
-            next_datetime = curr_datetime + pd.Timedelta(days=1)
-            df_t = df_m5[(df_m5.index >= curr_datetime) & (df_m5.index <= next_datetime)].copy()
+            while current_month <= 12:
+                if current_year == start_datetime.year and current_month == start_datetime.month:
+                    first_day_of_month = start_datetime.day
+                else:
+                    first_day_of_month = 1
+                    # print(f"first_day_of_month={first_day_of_month}")
+                if current_year == end_datetime.year and current_month == end_datetime.month:
+                    last_day_of_month = end_datetime.day
+                else:
+                    last_day = calendar.monthrange(current_year, current_month)[1]
+                    last_day_of_month = last_day
+                    # print(f"last_day_of_month={last_day_of_month}")
 
-            for i in range(1, len(df_t)):
-                prev_row = df_t.iloc[i-1]
-                curr_row = df_t.iloc[i]
+                sdatetime = datetime(year=current_year, month=current_month, day=first_day_of_month, hour=8, minute=55, second=0)
+                edatetime = datetime(year=current_year, month=current_month, day=last_day_of_month, hour=8, minute=55, second=0)
 
-                if position == 0 and prev_row['close'] <= prev_row['SMA20'] and curr_row['close'] >= curr_row['SMA20'] :
-                    buy_time = curr_row.name + pd.Timedelta(minutes=1)
-                    buy_price = df_m1.loc[buy_time, 'close']
-                    trade_flag = 'buy'
+                curr_datetime = sdatetime
+                while curr_datetime <= edatetime:
+                    next_datetime = curr_datetime + pd.Timedelta(days=1)
+                    df_t = df_m5[(df_m5.index >= curr_datetime) & (df_m5.index <= next_datetime)].copy()
+                    for i in range(1, len(df_t)):
+                        gcount += 1
+                        prev_row = df_t.iloc[i-1]
+                        curr_row = df_t.iloc[i]
+                        if position == 0 and prev_row['close'] <= prev_row['SMA20'] and curr_row['close'] >= curr_row['SMA20'] :
+                            buy_time = curr_row.name + pd.Timedelta(minutes=1)
+                            buy_price = df_m1.loc[buy_time, 'close']
+                            trade_flag = 'buy'
 
-                    volume = int(set_volume(capital, buy_price) * 1000) / 1000
-                    capital = capital - buy_price * volume - (buy_price * volume) * 0.0005
-                    curr_profit = 0.0
+                            volume = set_volume(capital, buy_price)
+                            capital = capital - (buy_price * volume + buy_price * volume * 0.0005)
+                            curr_profit = 0.0
 
-                    trade = [buy_time, trade_flag, buy_price, volume, capital, curr_profit]
-                    trades.append(trade)            
-                    # print(f"매수: {buy_time}, 가격: {buy_price}, 수량: {volume}, 잔고: {capital}")
-                    position = 1
-                    continue
+                            trade = [buy_time, trade_flag, buy_price, volume, capital, curr_profit]
+                            trades.append(trade)            
+                            # print(f"매수: {buy_time}, 가격: {buy_price}, 수량: {volume}, 잔고: {capital}")
+                            position = 1
+                            continue
+                        elif position == 1  and prev_row['close'] >= prev_row['SMA20'] and curr_row['close'] <= curr_row['SMA20'] :
+                            sell_time = curr_row.name + pd.Timedelta(minutes=1)
+                            sell_price = df_m1.loc[sell_time, 'close']
+                            trade_flag = 'sell'
+                            capital = capital + (sell_price * volume - sell_price * volume * 0.0005)
+                            curr_profit = capital - initial_capital
+                            if curr_profit < 0.0:
+                                curr_profit = 0.0
+                            else:
+                                kday = sell_time.day
+                                profit = [current_year, current_month, kday, curr_profit]
+                                profits.append(profit)
+                                capital = initial_capital
+                            trade = [sell_time, trade_flag, sell_price, volume, capital, curr_profit]
+                            trades.append(trade)
+                            # print(f"매도: {sell_time}, 가격: {sell_price}, 수량: {volume}, 잔고: {capital}, 수익: {curr_profit}")
+                            position = 0                    
+                            continue
+                    curr_datetime = next_datetime
 
-                elif position == 1  and prev_row['close'] >= prev_row['SMA20'] and curr_row['close'] <= curr_row['SMA20'] :
+                if position == 1:
                     sell_time = curr_row.name + pd.Timedelta(minutes=1)
                     sell_price = df_m1.loc[sell_time, 'close']
                     trade_flag = 'sell'
-                    capital = capital + sell_price * volume - (sell_price * volume) * 0.0005
+                    capital = capital + (sell_price * volume - sell_price * volume * 0.0005)
                     curr_profit = capital - initial_capital
-                    if curr_profit < 0.0:
+                    if curr_profit <= 0.0:
                         curr_profit = 0.0
+                    else:
+                        kday = sell_time.day
+                        profit = [current_year, current_month, kday, curr_profit]
+                        profits.append(profit)
+                        capital = initial_capital
                     trade = [sell_time, trade_flag, sell_price, volume, capital, curr_profit]
                     trades.append(trade)
                     # print(f"매도: {sell_time}, 가격: {sell_price}, 수량: {volume}, 잔고: {capital}, 수익: {curr_profit}")
+                    position = 0
 
-                    if capital > initial_capital:
-                        myprofit = [sell_time, float(curr_profit)]
-                        myprofits.append(myprofit)
-                        capital = initial_capital
-                    position = 0                    
-                    continue
+                if current_year == end_datetime.year and current_month == end_datetime.month:
+                    break
+                current_month += 1
 
-            curr_datetime = next_datetime
-
-        if position == 1:
-            sell_time = curr_row.name + pd.Timedelta(minutes=1)
-            sell_price = df_m1.loc[sell_time, 'close']
-            trade_flag = 'sell'
-            capital = capital + sell_price * volume - (sell_price * volume) * 0.0005
-            curr_profit = capital - initial_capital
-            trade = [sell_time, trade_flag, sell_price, volume, capital, curr_profit]
-            trades.append(trade)
-
-            if capital > initial_capital:
-                myprofit = [sell_time, float(curr_profit)]
-                myprofits.append(myprofit)
-                capital = initial_capital
-            position = 0
+            if current_year == end_datetime.year:
+                break
+            current_year += 1
 
         df = pd.DataFrame(trades, columns=['time', 'trade', 'price', 'volume', 'balance', 'profit'])
         df.to_csv(trade_output)
         
-        df = pd.DataFrame(myprofits, columns=['time', 'profit'])
+        df = pd.DataFrame(profits, columns=['year', 'month', 'day', 'profit'])
         df.to_csv(profit_output)
 
         print(f"[ {ct} / {tt} ] - {profit_output} 파일에 Profit 저장")
+        print(f"gcount = {gcount}")
 
         ct += 1
 
